ee2af3c7de896142e7d8556e93297d9e00e8ca4c
jjaixx <614872321@qq.com>
2020-06-16 14:32:44 +0800

    test

---
+++ src/main/java/com/jenkins/springcloud/SpringcloudApplication.java
@@ -0,0 +1,31 @@
+package com.jenkins.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@EnableAutoConfiguration
+@RestController
+@RequestMapping("admin/")
+public class SpringcloudApplication {
+    /**
+     * 用户首页，用于登录之后给用户看一些信息
+     * 信息可以从数据库中取，这边作为demo，暂时写死
+     * @return
+     */
+    @RequestMapping("home")
+    String home(HttpServletRequest request) {
+        String userName = "Jef";
+        int count = 100;
+        return "欢迎您" + userName + "，这是您的第 " + count + " 次登录";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudApplication.class, args);
+    }
+
+}
